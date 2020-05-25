1faab705dfcc4bf05ad5384215a9c32f52a50cce
tzbibi <894661727@qq.com>
2020-05-25 19:52:51 +0800

    实现自动轮播图

---
+++ app/src/main/java/com/example/ticketunion/ui/custom/AutoLooperViewPager.java
@@ -0,0 +1,10 @@
+package com.example.ticketunion.ui.custom;
+
+/**
+ * @ProjectName: TicketUnion
+ * @Author: Tz
+ * @CreateDate: 2020/5/25 17:46
+ * God bless my code!
+ */
+public class AutoLooperViewPager {
+}
